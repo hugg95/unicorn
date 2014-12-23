f13249bfc65656e52c32317536be666fdeafc07a
victor <lncwwn@gmail.com>
2014-12-23 23:56:12 +0800

    added spiderSeedConfig as a auto injected spring bean

---
+++ src/main/java/org/lncwwn/unicorn/conf/SpiderSeedConfig.java
@@ -0,0 +1,29 @@
+package org.lncwwn.unicorn.conf;
+
+import javax.annotation.PostConstruct;
+import java.util.Set;
+
+/**
+ * config seed for spider
+ * Created by victor on 12/23/14.
+ */
+public class SpiderSeedConfig {
+
+    private Set<String> seeds;
+
+    public Set<String> getSeeds() {
+        return seeds;
+    }
+
+    public void setSeeds( Set<String> seeds ) {
+        this.seeds = seeds;
+    }
+
+    // for test, no practical use
+    @PostConstruct
+    public void printSeed() {
+        for (String seed: seeds) {
+            System.out.println(seed);
+        }
+    }
+}
